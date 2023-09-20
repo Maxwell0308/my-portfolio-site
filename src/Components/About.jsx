e39282e136ce675bfc4d4e8b0deef46e53fbaad7
Maxwell Hilary <74822038+Maxwell0308@users.noreply.github.com>
2023-09-20 02:59:41 +0000

    Yes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/About.svg";
+import image from "../images/About2.jpg";
 
 const imageAltText = "kinda cool";
 
